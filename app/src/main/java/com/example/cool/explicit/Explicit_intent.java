c3d23e7a40ca0abb70614c81df3c2033f0f4a837
satyam pandey <39619114+Satyamp32@users.noreply.github.com>
2018-06-22 14:28:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cool/explicit/Explicit_intent.java
@@ -0,0 +1,13 @@
+package com.example.cool.explicit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Explicit_intent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explicit_intent);
+    }
+}
